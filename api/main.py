063ce14e85b507de7201663acc33699032a58757
vinayak-shree <142142244+vinayak-shree@users.noreply.github.com>
2026-04-08 16:13:47 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -50,5 +50,5 @@ async def predict(
 }
     #to run this server use uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app , host = "localhost" , port = 8000)
+    uvicorn.run(app , host = "0.0.0.0" , port = 8000)
 
