077b940a3998a9c7f056991b2337a979ae1f0a8c
vinayak-shree <142142244+vinayak-shree@users.noreply.github.com>
2026-04-10 19:03:52 +0530

    Update header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -8,7 +8,7 @@ function Header() {
         const formData = new FormData()
         formData.append("file",file)
 
-        const response = await fetch("http://127.0.0.1:8000/predict" , {
+        const response = await fetch("https://potatoe-disease-fastapi-url.onrender.com" , {
             method: "POST",
             body: formData,
         })
